60688d97c574b7f26b153a27bbf03655e85f018f
iamxxx777 <iamxxx333@gmail.com>
2022-05-13 14:46:35 +0100

    added font link

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=League+Spartan:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
                 <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
             </Head>
             <body>
